10ed6f5d61d1ede549295c905636012d0f2946ed
Hermann Hueck <hermann.hueck@mailbox.org>
2020-04-28 12:11:44 +0200

    reformat code

--- src/main/scala/doc/datatypes/io/App05eMyAsyncEffects.scala
+++ src/main/scala/doc/datatypes/io/App05eMyAsyncEffects.scala
@@ -10,7 +10,7 @@ object App05eMyAsyncEffects extends App {
     def isEven: Boolean = num % 2 == 0
   }
 
-  type ErrorOr[A] = Either[Throwable, A]
+  type ErrorOr[A]  = Either[Throwable, A]
   type Callback[A] = ErrorOr[A] => Unit
 
   def checkEven(num: Int): ErrorOr[Int] = {
@@ -18,20 +18,23 @@ object App05eMyAsyncEffects extends App {
     print(s"   Computing in thread: ${Thread.currentThread().getName} ...  ")
     Thread.sleep(3000L)
 
-    val result = if (num.isEven) Right(num) else Left(new IllegalArgumentException(s"$num is not even"))
+    val result =
+      if (num.isEven)
+        Right(num)
+      else
+        Left(new IllegalArgumentException(s"$num is not even"))
 
     println(s"done: result = $result")
     result
   }
 
-  val ioa: IO[Int] = IO.async { cb: Callback[Int] =>
-    cb(checkEven(10))
-  }
+  val ioa: IO[Int] = IO.async { cb: Callback[Int] => cb(checkEven(10)) }
 
-  val callback: Callback[Int] = e => println(e match {
-    case Left(throwable: Throwable) => throwable.toString
-    case Right(num) => s"$num is even"
-  })
+  val callback: Callback[Int] = e =>
+    println(e match {
+      case Left(throwable: Throwable) => throwable.toString
+      case Right(num)                 => s"$num is even"
+    })
 
   println(s"Main: ${Thread.currentThread().getName}")
   println("Running async ...")
--- src/main/scala/doc/datatypes/io/App05fMyAsyncEffects.scala
+++ src/main/scala/doc/datatypes/io/App05fMyAsyncEffects.scala
@@ -10,7 +10,7 @@ object App05fMyAsyncEffects extends App {
     def isEven: Boolean = num % 2 == 0
   }
 
-  type ErrorOr[A] = Either[Throwable, A]
+  type ErrorOr[A]  = Either[Throwable, A]
   type Callback[A] = ErrorOr[A] => Unit
 
   def checkEven(num: Int): ErrorOr[Int] = {
@@ -18,19 +18,22 @@ object App05fMyAsyncEffects extends App {
     print(s"   Computing in thread: ${Thread.currentThread().getName} ...  ")
     Thread.sleep(3000L)
 
-    val result = if (num.isEven) Right(num) else Left(new IllegalArgumentException(s"$num is not even"))
+    val result =
+      if (num.isEven)
+        Right(num)
+      else
+        Left(new IllegalArgumentException(s"$num is not even"))
 
     println(s"done: result = $result")
     result
   }
 
-  def computeAsync[A](thunk: => ErrorOr[A]): IO[A] = IO.async { cb: Callback[A] =>
-    cb(thunk)
-  }
+  def computeAsync[A](thunk: => ErrorOr[A]): IO[A] = IO.async { cb: Callback[A] => cb(thunk) }
 
   val ioa: IO[Int] = computeAsync { checkEven(10) }
 
-  val callback: Callback[Int] = result => println(result.fold(ex => ex.toString, num => s"$num is even"))
+  val callback: Callback[Int] =
+    result => println(result.fold(ex => ex.toString, num => s"$num is even"))
 
   println(s"Main: ${Thread.currentThread().getName}")
   println("Running async ...")
